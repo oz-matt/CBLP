1dcda727dddc436223c66d662486fe9840675d2e
ozoralabs <matt.morra@ozoralabs.com>
2019-08-28 19:38:32 -0400

    linux c prog updated

--- DE10_NANO_SoC_GHRD/HPS_FPGA_LED/mainold.c
+++ DE10_NANO_SoC_GHRD/HPS_FPGA_LED/mainold.c
@@ -12,38 +12,17 @@
 #define HW_REGS_BASE ( 0xFF200000 )
 #define HW_REGS_SPAN ( 0x001000 )
 
-int toInt(char a[]) {
-  int c, sign, offset, n;
- 
-  if (a[0] == '-') {  // Handle negative integers
-    sign = -1;
-  }
- 
-  if (sign == -1) {  // Set starting position to convert
-    offset = 1;
-  }
-  else {
-    offset = 0;
-  }
- 
-  n = 0;
- 
-  for (c = offset; a[c] != '\0'; c++) {
-    n = n * 10 + a[c] - '0';
-  }
- 
-  if (sign == -1) {
-    n = -n;
-  }
- 
-  return n;
-}
-
-int main(int argc, char *argv[]) {
-
+int main() {
+usleep( 100*1000 );
+printf("1\n");
+usleep( 100*1000 );
 	void *virtual_base = 0;
 	uint32_t* h2p_lw_led_addr = 0;
 	int fd;
+	int counter = 0;
+	usleep( 100*1000 );
+printf("2\n");
+usleep( 100*1000 );
 	// map the address space for the LED registers into user space so we can interact with them.
 	// we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
 
@@ -51,27 +30,52 @@ int main(int argc, char *argv[]) {
 		printf( "ERROR: could not open \"/dev/mem\"...\n" );
 		return( 1 );
 	}
-	
+	usleep( 100*1000 );
+printf("3\n");
+usleep( 100*1000 );
 	virtual_base = (uint32_t*)mmap( NULL, HW_REGS_SPAN, PROT_WRITE, MAP_SHARED, fd, HW_REGS_BASE );
-
+usleep( 100*1000 );
+	printf("4\n");
+	usleep( 100*1000 );
 	if( virtual_base == MAP_FAILED ) {
 		printf( "ERROR: mmap() failed...\n" );
 		close( fd );
 		return( 1 );
 	}
-	
+	usleep( 100*1000 );
+	printf("5\n");
+	usleep( 100*1000 );
 	h2p_lw_led_addr=(uint32_t*)(virtual_base + CUSTOM_LEDS_0_BASE);
+	usleep( 100*1000 );
+printf("6\n");
+	// toggle the LEDs a bit
+usleep( 100*1000 );
 
-	
-	
-	//!
-	uint32_t freqarg = (uint32_t) toInt(argv[1]);
-	printf( "freqarg: %i", freqarg);
-	*h2p_lw_led_addr = freqarg; 
-	
-	
-	
-	
+*h2p_lw_led_addr = (uint32_t)0x00; 
+
+	while(1) {
+		printf("7\n");
+		// control led
+		*h2p_lw_led_addr |= (uint32_t)0x10; 
+
+		// wait 100ms
+		usleep( 100*1000 );
+		*h2p_lw_led_addr &= (uint32_t)0xFFFFFFEF; 
+		
+		if(*h2p_lw_led_addr & 0x01)printf("1");
+		
+
+		// wait 100ms
+		usleep( 100*1000 );
+		
+		if(counter++ >= 255)
+			counter = 0;
+			
+		printf("Counter: %i\n", counter);
+			
+		
+	} 
+
 	// clean up our memory mapping and exit
 	
 	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
